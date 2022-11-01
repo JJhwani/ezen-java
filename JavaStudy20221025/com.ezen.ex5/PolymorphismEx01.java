0edcc9e72c957c13e1fe45c7197126b7cda35d3b
JJhwani <116856675+JJhwani@users.noreply.github.com>
2022-11-01 11:22:40 +0900

    abstract class, Inheritance, Overriding

---
+++ JavaStudy20221025/com.ezen.ex5/PolymorphismEx01.java
@@ -0,0 +1,56 @@
+package com.ezen.ex05;
+
+public class PolymorphismEx01 {
+
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.work();
+		p = new OfficeJob();
+		p.work();
+		
+		p = new Student();
+		p.work();
+		
+		p = new SportsMan();
+		p.work();
+		//p.run();
+
+	}
+
+}
+
+class Person{
+	private String name;
+	private int age;
+	void work() {
+		System.out.println("사람은 일한다.");
+	}
+	//void run() {}
+}
+
+class Student extends Person{ //학생
+	void work() {
+		System.out.println("공부한다.");
+	}
+}
+
+class SportsMan extends Person{ //운동선수
+	void work() {
+		System.out.println("운동한다.");
+	}
+	void run() {
+		System.out.println("달린다.");
+	}
+}
+
+class OfficeJob extends Person{ //사무직
+	void work() {
+		System.out.println("사무실에서 일한다.");
+	}
+}
+
+class Worker extends Person{ //노동자
+	void work() {
+		System.out.println("현장에서 일한다.");
+	}
+}
