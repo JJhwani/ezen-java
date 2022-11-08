b5791c50ff0b15a666ac20aa22f84a904f3e51aa
JJhwani <116856675+JJhwani@users.noreply.github.com>
2022-11-08 15:48:38 +0900

    Java Method Exercise 1

---
+++ JavaStudy20221107/com.ezen.ex01/Ex01.java
@@ -0,0 +1,24 @@
+package com.exen.ex01;
+
+import java.util.*;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이디를 입력하십시오: ");
+		String name = sc.nextLine();
+		System.out.println(inputName(name));
+	}
+
+	static String inputName(String name) {
+		List<String> nameList = new ArrayList<>(Arrays.asList("min10013", "smb10013", "qortnals"));
+		for (String i : nameList) {
+			if (name.equals(i))
+				return "중복입니다.";
+		}
+		return "가능합니다.";
+	}
+
+}
