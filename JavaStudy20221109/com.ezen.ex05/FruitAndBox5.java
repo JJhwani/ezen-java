8d6f195f39220ed08c21f58c7e102ef970ca670a
JJhwani <116856675+JJhwani@users.noreply.github.com>
2022-11-10 15:20:04 +0900

    Generics

---
+++ JavaStudy20221109/com.ezen.ex05/FruitAndBox5.java
@@ -0,0 +1,58 @@
+package com.ezen.ex05;
+
+class Fruit{
+	@Override
+	public String toString() {
+		return "과일이다임뫄~~~";
+	}
+}
+class Apple extends Fruit{
+	@Override
+	public String toString() {
+		return "I am an Apple.";
+	}
+}
+class Orange extends Fruit{
+	@Override
+	public String toString() {
+		return "I am an Orange.";
+	}
+}
+class Person{
+	@Override
+	public String toString() {
+		return "사람이다.";
+	}
+}
+
+class Box<T extends Fruit>{
+	private T ap;
+	void set(T a) {
+		ap=a;
+	}
+	T get() {
+		return ap;
+	}
+}
+
+public class FruitAndBox5 {
+	public static void main(String[] args) {
+		
+		Box<Apple> abox = new Box();		
+		abox.set(new Apple());
+		Apple ap = abox.get();
+		System.out.println(ap);
+		
+		Box<Orange> obox = new Box();
+		obox.set(new Orange());
+		Orange or = obox.get();
+		System.out.println(or);
+		
+//		Box<Person> pbox = new Box();
+//		pbox.set(new Person());
+//		Person p = pbox.get();
+//		System.out.println(p);
+		
+	}
+
+}
