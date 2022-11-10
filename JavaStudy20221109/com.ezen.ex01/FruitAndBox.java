ed2de8c24e12e676ea2238696ee1fdb71c5c8644
JJhwani <116856675+JJhwani@users.noreply.github.com>
2022-11-10 15:18:10 +0900

    Generics

---
+++ JavaStudy20221109/com.ezen.ex01/FruitAndBox.java
@@ -0,0 +1,52 @@
+package com.ezen.ex01;
+
+class Apple{
+	@Override
+	public String toString() {
+		return "I am an Apple.";
+	}
+}
+
+class Orange{
+	@Override
+	public String toString() {
+		return "I am an Orange.";
+	}
+}
+
+class AppleBox{
+	private Apple ap;
+	void set(Apple a) {
+		ap=a;
+	}
+	Apple get() {
+		return ap;
+	}
+}
+
+class OrangeBox{
+	private Orange or;
+	void set(Orange o) {
+		or=o;
+	}
+	Orange get() {
+		return or;
+	}
+}
+
+public class FruitAndBox {
+	public static void main(String[] args) {
+		AppleBox aBox = new AppleBox();
+		OrangeBox oBox = new OrangeBox();
+		
+		aBox.set(new Apple());
+		oBox.set(new Orange());
+		
+		Apple a = aBox.get();
+		System.out.println(a);
+		
+		Orange o = oBox.get();
+		System.out.println(o);
+	}
+
+}
