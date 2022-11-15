4616465ef0e8305bdc0d7258ccce88d360eeb54f
JJhwani <116856675+JJhwani@users.noreply.github.com>
2022-11-15 09:41:30 +0900

    JDBC

---
+++ JavaStudy20221111/DBMemberEx01.java
@@ -0,0 +1,55 @@
+package com.ezen.ex01;
+
+import java.sql.*;
+
+public class DBMemberEx01 {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String user = "scott";
+	private static final String pwd = "1234";
+	
+	public static void main(String[] args) {
+		Connection conn;
+		Statement stmt;
+		ResultSet rs;
+		
+		try {
+			Class.forName(driver);
+			System.out.println("드라이브 로드 성공");
+			
+			conn = DriverManager.getConnection(url,user,pwd);
+			System.out.println("Connection 연결 성공");
+			
+			stmt = conn.createStatement();
+			
+			String sql = "insert into member values('0006','이예찬',170,98,55)";
+			
+			int num = stmt.executeUpdate(sql);
+			System.out.println("asdfasdf");
+			System.out.println("insert 성공 : "+num);
+			System.out.println("---------------------------------------");
+			
+			String sql2 = "select*from member order by id";
+			rs = stmt.executeQuery(sql2);
+			System.out.println("아이디    이름    키    몸무게    나이");
+			
+			while(rs.next()) {
+				System.out.print(rs.getString("id")+"\t");
+				System.out.print(rs.getString("name")+"\t");
+				System.out.print(rs.getInt("height")+"\t");
+				System.out.print(rs.getInt("weight")+"\t");
+				System.out.print(rs.getInt("age")+"\t");
+				System.out.println();				
+			}
+			
+			String sql3 = "update member set name = '윤재문' where id = '0003'";
+			int num2 = stmt.executeUpdate(sql3);
+			System.out.println("update 성공 : "+num2);
+						
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
